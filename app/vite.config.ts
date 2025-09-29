73b4c5bf5d6772a72efe0f7c8518d82751b2de36
montaguez <lpirqahye3084@hotmail.com>
2025-09-29 17:55:55 +0800

    use react

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,20 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    global: 'globalThis',
-  },
-  resolve: {
-    alias: {
-      buffer: 'buffer',
-      crypto: 'crypto-browserify',
-      stream: 'stream-browserify',
-      util: 'util',
-    },
-  },
-  optimizeDeps: {
-    include: ['buffer', 'crypto-browserify', 'stream-browserify', 'util'],
-  },
 })
